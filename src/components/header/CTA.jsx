55f2d0d457ddc2330e52b5ec2519e56c21ca5ce3
Felipe <garciasuezfelipe@gmail.com>
2022-11-29 22:36:05 -0300

    CV load

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import CV_SPANISH from '../../assets/Felipe Garcia ESPAÑOL CV.pdf'
+import CV_SPANISH from '../../assets/Felipe Garcia ENGLISH CV.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-       <a href={CV} download className='btn'>Download CV</a>
+       <a href={CV_ENGLISH} download className='btn'>Download English CV</a>
+       <a href={CV_SPANISH} download className='btn'>Download Spanish CV</a>
        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
